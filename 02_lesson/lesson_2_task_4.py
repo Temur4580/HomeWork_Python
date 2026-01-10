d25b65b1f17e94ada67f108a1c5d9821c31c90b5
Temur4580 <bilalovtemur32@gmail.com>
2026-01-10 18:19:20 +0300

    Задача FizzBuzz

---
+++ 02_lesson/lesson_2_task_4.py
@@ -0,0 +1,16 @@
+def fizz_buzz(n):
+    """Печатает FizzBuzz последовательность от 1 до n"""
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+
+print("fizz_buzz(17) →")
+fizz_buzz(17)
